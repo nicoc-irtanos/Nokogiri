00d3fe127f4302f98edd68b34006b2bd752e47ff
nicolasculerier <nicolas.culerier@laposte.net>
2018-07-12 19:39:44 +0200

    Commit route des députés amélioré

--- route_des_deputes.rb
+++ route_des_deputes.rb
@@ -10,24 +10,23 @@ require 'open-uri'
 
 @arraydehashs = []
 
-def get_mps_names()
+def get_mps_names
   webpage = Nokogiri::HTML(open('http://www2.assemblee-nationale.fr/deputes/liste/alphabetique'))
   webpage.xpath('//ul[@class = "col3"]/li/a').each do |bli|
     @names << bli.text
   end
 end
 
-def orga()
+def orga
   @names.map{|i| i.delete_prefix!("M. ")}
   @names.map{|i| i.delete_prefix!("Mme ")}
   @names.each{|i| array = i.split(" ")
-
-  @first_names <<  array[0]
-  @last_names << array.drop(1).join(" ")
+  @first_names<<  array[0]
+  @last_names<< array.drop(1).join(" ")
   }
 end
 
-def get_urls()
+def get_urls
   page = Nokogiri::HTML(open('http://www2.assemblee-nationale.fr/deputes/liste/alphabetique'))
   page.xpath('//ul[@class = "col3"]/li/a/@href').each do |bam|
     @urls << bam.text
@@ -36,20 +35,24 @@ end
 
 def format_urls
   @urls.each do |i|
-  i.insert(0, "http://www2.assemblee-nationale.fr")
+  i.insert(0, 'http://www2.assemblee-nationale.fr')
   end
 end
 
-def get_mps_emails()
+def get_mps_emails
   @urls.each do |k|
     noko = Nokogiri::HTML(open(k))
-    noko.xpath('/html/body/div[3]/div/div/div/section[1]/div/article/div[3]/div/dl/dd[4]/ul/li/a').each do |u|
+    noko.xpath('/html/body/div[3]/div/div/div/section[1]/div/article/div[3]/div/dl/dd[4]/ul/li[1]/a/@href').each do |u|
+      puts u.text
       @emails << u.text
     end
   end
 end
 
-get_mps_names()
-orga()
-get_urls()
-format_urls()
+puts @emails
+
+get_mps_names
+orga
+get_urls
+format_urls
+get_mps_emails
